b634fb147ced72084bf7777afb053f054e74da32
blahu <blahu@users.noreply.github.com>
2014-08-03 15:46:51 +0100

    class4 ex1.py

---
+++ class4/ex1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# 
+# Class     4
+# Exercise  1
+# Author    Mateusz Blaszczyk
+import sys
+
+print sys.argv[0] + "....begin\n\n"
+
+
+
+
+
+
+print sys.argv[0] + "....end\n\n"
