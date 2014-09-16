b34fb2758a38869fc3b18d8792f53ae38a6f623b
blahu <blahu@users.noreply.github.com>
2014-09-16 23:17:20 +0100

    added class2/ex1, small update to  cl1ex3b

--- AppliedPythonCourse/SNMP/ROUTERS.py
+++ AppliedPythonCourse/SNMP/ROUTERS.py
@@ -4,16 +4,25 @@ ROUTERS = {
         'IP' : '50.242.94.227',
         'SNMP Port' : 7961,
         'SNMP Community' : 'galileo',
+        'snmp_v' : 3,
     },
     'pynet-rtr2' : {
         'IP' : '50.242.94.227',
         'SNMP Port' : 8061,
         'SNMP Community' : 'galileo',
+        'snmp_v' : 3,
     },
-    'py_router' : {
+    'pymat_rtr1' : {
         'IP' : '10.1.1.1',
         'SNMP Port' : 161,
         'SNMP Community' : 'galileo',
+        'snmp_v' : 2,
+    },
+    'pymat_rtr2' : {
+        'IP' : '10.1.1.2',
+        'SNMP Port' : 161,
+        'SNMP Community' : 'galileo',
+        'snmp_v' : 3,
     },
 }
 
--- AppliedPythonCourse/SNMP/cl1ex3b.py
+++ AppliedPythonCourse/SNMP/cl1ex3b.py
@@ -11,51 +11,50 @@ from ROUTERS import ROUTERS
 from OIDS import OIDS
 
 for router, router_data in ROUTERS.items():
-    if router not in sys.argv[1]:
-        continue
-    print
-    print router
-    
-    # create snmp_device tuple
-    snmp_device = ( router_data['IP'], router_data['SNMP Community'], router_data['SNMP Port'])
-    
-    # get snmp ccmHistoryRunningLastChanged
-    snmp_data_RunningLastChanged = snmp_get_oid (snmp_device,OIDS['ccmHistoryRunningLastChanged']) 
-    snmp_data_RunningLastSaved   = snmp_get_oid (snmp_device,OIDS['ccmHistoryRunningLastSaved']) 
-    snmp_data_StartupLastChanged = snmp_get_oid (snmp_device,OIDS['ccmHistoryStartupLastChanged']) 
+    if sys.argv[1] in router:
+        print
+        print router
+        
+        # create snmp_device tuple
+        snmp_device = ( router_data['IP'], router_data['SNMP Community'], router_data['SNMP Port'])
+        
+        # get snmp ccmHistoryRunningLastChanged
+        snmp_data_RunningLastChanged = snmp_get_oid (snmp_device,OIDS['ccmHistoryRunningLastChanged']) 
+        snmp_data_RunningLastSaved   = snmp_get_oid (snmp_device,OIDS['ccmHistoryRunningLastSaved']) 
+        snmp_data_StartupLastChanged = snmp_get_oid (snmp_device,OIDS['ccmHistoryStartupLastChanged']) 
 
-    # get snmp uptime
-    snmp_data_uptime = snmp_get_oid (snmp_device,OIDS['sysUpTime']) 
+        # get snmp uptime
+        snmp_data_uptime = snmp_get_oid (snmp_device,OIDS['sysUpTime']) 
 
-    div = 100
-    running_last_changed_ticks = int (snmp_extract (snmp_data_RunningLastChanged)) / div
-    running_last_saved_ticks   = int (snmp_extract (snmp_data_RunningLastSaved))   / div
-    startup_last_changed_ticks = int (snmp_extract (snmp_data_StartupLastChanged)) / div
-    uptime_ticks               = int (snmp_extract (snmp_data_uptime))             / div
+        div = 100
+        running_last_changed_ticks = int (snmp_extract (snmp_data_RunningLastChanged)) / div
+        running_last_saved_ticks   = int (snmp_extract (snmp_data_RunningLastSaved))   / div
+        startup_last_changed_ticks = int (snmp_extract (snmp_data_StartupLastChanged)) / div
+        uptime_ticks               = int (snmp_extract (snmp_data_uptime))             / div
 
-    print "{:10} uptime               ".format (uptime_ticks               )
-    print "{:10} running last_changed ".format (running_last_changed_ticks )
-    print "{:10} running last_saved   ".format (running_last_saved_ticks   )
-    print "{:10} startup last_changed ".format (startup_last_changed_ticks )
+        print "{:10} uptime               ".format (uptime_ticks               )
+        print "{:10} running last_changed ".format (running_last_changed_ticks )
+        print "{:10} running last_saved   ".format (running_last_saved_ticks   )
+        print "{:10} startup last_changed ".format (startup_last_changed_ticks )
 
 
-    time_since_startup_saved = (uptime_ticks - startup_last_changed_ticks ) 
-    print "Time since startup saved {}".format(time_since_startup_saved)
+        time_since_startup_saved = (uptime_ticks - startup_last_changed_ticks ) 
+        print "Time since startup saved {}".format(time_since_startup_saved)
 
-    time_since_running_changed = (uptime_ticks - running_last_changed_ticks) 
-    print "Time since running changed {}".format(time_since_running_changed)
+        time_since_running_changed = (uptime_ticks - running_last_changed_ticks) 
+        print "Time since running changed {}".format(time_since_running_changed)
 
-    time_since_running_saved = (uptime_ticks - running_last_saved_ticks ) 
-    print "Time since running saved {}".format(time_since_running_saved)
-    
-    if running_last_changed_ticks > running_last_saved_ticks:
-        print "Running Config has been changed"
-    else:
-        print "Running Config has not been changed since last Running Config save"
-        if startup_last_changed_ticks > running_last_changed_ticks:
-            print "Startup Config has been saved since last Running Config change"
+        time_since_running_saved = (uptime_ticks - running_last_saved_ticks ) 
+        print "Time since running saved {}".format(time_since_running_saved)
+        
+        if running_last_changed_ticks > running_last_saved_ticks:
+            print "Running Config has been changed"
         else:
-            print "Startup Config has not been saved since last Running Config change"
+            print "Running Config has not been changed since last Running Config save"
+            if startup_last_changed_ticks > running_last_changed_ticks:
+                print "Startup Config has been saved since last Running Config change"
+            else:
+                print "Startup Config has not been saved since last Running Config change"
 
 
 print
