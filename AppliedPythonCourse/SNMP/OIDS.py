74485db2a39bb53f3aa012c38b99fc2b758567cd
blahu <blahu@users.noreply.github.com>
2014-09-17 20:42:17 +0100

    started cl2 ex2

--- AppliedPythonCourse/SNMP/OIDS.py
+++ AppliedPythonCourse/SNMP/OIDS.py
@@ -5,5 +5,11 @@ OIDS={
     'ccmHistoryRunningLastChanged'  :'1.3.6.1.4.1.9.9.43.1.1.1.0',
     'ccmHistoryRunningLastSaved'    :'1.3.6.1.4.1.9.9.43.1.1.2.0',
     'ccmHistoryStartupLastChanged'  :'1.3.6.1.4.1.9.9.43.1.1.3.0',
+    # ifTable
+    'ifDescr'                       :'1.3.6.1.2.1.2.2.1.2',
+    'ifInOctets'                    :'1.3.6.1.2.1.2.2.1.10',
+    'ifInUcastPkts'                 :'1.3.6.1.2.1.2.2.1.11',
+    'ifOutOctets'                   :'1.3.6.1.2.1.2.2.1.16',
+    'ifOutUcastPkts'                :'1.3.6.1.2.1.2.2.1.17',
 }
 # If the value of ccmHistoryRunningLastChanged is greater than ccmHistoryRunningLastSaved, the configuration has been changed but not saved
--- AppliedPythonCourse/SNMP/ROUTERS.py
+++ AppliedPythonCourse/SNMP/ROUTERS.py
@@ -5,12 +5,14 @@ ROUTERS = {
         'SNMP Port' : 7961,
         'SNMP Community' : 'galileo',
         'snmp_v' : 3,
+        'snmp_user' : ('pysnmp', 'galileo1', 'galileo1'),
     },
     'pynet-rtr2' : {
         'IP' : '50.242.94.227',
         'SNMP Port' : 8061,
         'SNMP Community' : 'galileo',
         'snmp_v' : 3,
+        'snmp_user' : ('pysnmp', 'galileo1', 'galileo1'),
     },
     'pymat_rtr1' : {
         'IP' : '10.1.1.1',
@@ -23,6 +25,7 @@ ROUTERS = {
         'SNMP Port' : 161,
         'SNMP Community' : 'galileo',
         'snmp_v' : 3,
+        'snmp_user' : ('pysnmp', 'galileo1', 'galileo1'),
     },
 }
 
